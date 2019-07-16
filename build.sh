9f690b63f5ac869a6d34c53b1142c86551704d82
idl3r <idler1984@gmail.com>
2019-07-16 19:52:54 +0800

    update

--- build.sh
+++ build.sh
@@ -3,5 +3,14 @@
 # Unpack
 ./unpack.sh
 
-# RUn test
-./run_vmlinux.sh 2>log.txt
+# Run test
+./run_vmlinux.sh 2>&1 | tee log.txt
+
+# Check results
+grep -P "^\!\!\!TEST \d+ FAILED\!\!\!$" log.txt
+if [ $? -eq 0 ]; then
+    echo "Failure detected"
+    exit 1
+fi
+
+exit 0
